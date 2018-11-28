6276924fb8952fef32f08db6f70226c8d5a3f3f6
Jarod-MIDY <44644746+Jarod-MIDY@users.noreply.github.com>
2018-11-28 10:55:16 +0100

    ajout d'une génération de label

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//JMI label code no css
+function boutonLabel() {
+  $('#droite').append('<div id="add-label"></div>');
+  $('#add-label').append('<p>Texte du label :</p>').append('<input id="label-val" type="text" name="" value="">').append('<button onclick="okLabel()">Ok</button>');
+  $('#label-btn').prop('disabled', true);
+}
+function okLabel() {
+  let valLab = $('#label-val').val();
+  $('#gauche').append('<span>' + valLab + ' </span>');
+  $('#add-label').remove();
+  $('#label-btn').prop('disabled', false);
+}
 
