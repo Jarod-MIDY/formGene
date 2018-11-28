43dca43fefee4ac13b423783ac96c04ab4c8a530
Jarod-MIDY <44644746+Jarod-MIDY@users.noreply.github.com>
2018-11-28 11:40:23 +0100

    Ajout de css
    border-top + marge entre block de bouton et block label
    Font-size + paddind sur span créé

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-//JMI label code no css
+//JMI label code
 function boutonLabel() {
   $('#droite').append('<div id="add-label"></div>');
+  $('#add-label').css({"border-top": "1px solid black","margin-top":"15px"});
   $('#add-label').append('<p>Texte du label :</p>').append('<input id="label-val" type="text" name="" value="">').append('<button onclick="okLabel()">Ok</button>');
   $('#label-btn').prop('disabled', true);
 }
 function okLabel() {
   let valLab = $('#label-val').val();
   if (valLab) {
-    $('#gauche').append('<span>' + valLab + ' </span>');
+    $('#gauche').append('<span class="label-span">' + valLab + ' </span>');
   }
+  $('.label-span').css({"padding":"10px","font-size":"1.5em","display":"inline-block"});
   $('#add-label').remove();
   $('#label-btn').prop('disabled', false);
 }
